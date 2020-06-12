80c5c149d5b7e2fbcfbbd41fd638e4b73f6c5c60
uzunhair <uzunhair@yandex.ru>
2020-06-12 07:40:15 +0300

    Убрал лишний код

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,18 @@
 // Bootstrap
 
-require('../../node_modules/popper.js/dist/umd/popper.min');
-require('../../node_modules/bootstrap/js/dist/util');
-require('../../node_modules/bootstrap/js/dist/alert');
-require('../../node_modules/bootstrap/js/dist/button');
-require('../../node_modules/bootstrap/js/dist/carousel');
-require('../../node_modules/bootstrap/js/dist/collapse');
-require('../../node_modules/bootstrap/js/dist/dropdown');
-require('../../node_modules/bootstrap/js/dist/modal');
-require('../../node_modules/bootstrap/js/dist/scrollspy');
-require('../../node_modules/bootstrap/js/dist/tab');
-require('../../node_modules/bootstrap/js/dist/tooltip');
-require('../../node_modules/bootstrap/js/dist/popover');
-require('../../node_modules/bootstrap/js/dist/toast');
+// require('../../node_modules/popper.js/dist/umd/popper.min');
+// require('../../node_modules/bootstrap/js/dist/util');
+// require('../../node_modules/bootstrap/js/dist/alert');
+// require('../../node_modules/bootstrap/js/dist/button');
+// require('../../node_modules/bootstrap/js/dist/carousel');
+// require('../../node_modules/bootstrap/js/dist/collapse');
+// require('../../node_modules/bootstrap/js/dist/dropdown');
+// require('../../node_modules/bootstrap/js/dist/modal');
+// require('../../node_modules/bootstrap/js/dist/scrollspy');
+// require('../../node_modules/bootstrap/js/dist/tab');
+// require('../../node_modules/bootstrap/js/dist/tooltip');
+// require('../../node_modules/bootstrap/js/dist/popover');
+// require('../../node_modules/bootstrap/js/dist/toast');
 
 // Bem
 // require('../blocks/components/_components');
